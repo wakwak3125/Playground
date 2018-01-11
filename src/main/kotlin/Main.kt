b21be1911fda160b383d78bbd1cae1b3ebb8eb08
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2018-01-11 22:52:44 +0900

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,3 @@
+fun main(args: Array<String>) {
+    print("THIS IS MAIN FUNCTION")
+}
---
+++ src/main/kotlin/Playground.kt
@@ -0,0 +1,9 @@
+import arrow.core.Option
+import arrow.core.Some
+
+class Playground {
+
+    fun run() {
+
+    }
+}
